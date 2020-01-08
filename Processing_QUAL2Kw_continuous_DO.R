7a775dc2d39de0711d13a23b856d535393753eb7
dsobota <sobota.daniel@deq.state.or.us>
2020-01-08 13:21:57 -0800

    Update with test of binomial test

--- Processing_QUAL2Kw_continuous_DO.R
+++ Processing_QUAL2Kw_continuous_DO.R
@@ -10,7 +10,7 @@ source("Extracting_QUAL2Kw_continuous_DO.R")
 # Get dataframe into date and numeric format----
 # Get data into numeric format
 Q2K.df.num <- as.data.frame(
-                            apply(Q2K.df[ , -which(names(Q2K.df) == "")],
+                            apply(subset(Q2K.df, select = c("Reach", "Time", "Dissolved Oygen")),
                                   2,
                                   as.numeric
                                   )
@@ -18,5 +18,21 @@ Q2K.df.num <- as.data.frame(
 
 # Need Reach to be an integer
 Q2K.df.num$Reach <- as.integer(Q2K.df.num$Reach)
+Q2K.df.num$MeetDO <- ifelse(Q2K.df.num$`Dissolved Oygen` < 8, FALSE, TRUE)
+
 
 # Apply binomial test to model output----
+# First define parameters of the test
+
+prob.meet <- 0.9
+meet.confid <- 0.9
+
+# Performing the binomial test by Reach
+Q2K.binom.results <- binom.test(x = sum(Q2K.df.num[Q2K.df.num$Reach == 1, ]$MeetDO),
+                                n = length(Q2K.df.num[Q2K.df.num$Reach == 1, ]$MeetDO),
+                                p = prob.meet,
+                                alternative = "two.sided",
+                                conf.level = meet.confid
+                                )
+
+Q2K.binom.results[[3]]
